bf4b8cacf854ad5b77f99581575e5860ff649c8f
harishraghav03 <harishraghav@yectra.com>
2024-08-22 12:41:57 +0530

    Updated

--- function_app.py
+++ function_app.py
@@ -1,12 +1,12 @@
 import azure.functions as func
 from wrapper_functions.imageCompressor import app as imagecompressor
- 
+
 # Create the main FunctionApp instance
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
- 
+
 # Middleware for file conversion
 async def image_compressor(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     return await func.AsgiMiddleware(imagecompressor).handle_async(req, context)
- 
+
 # Register the routes with the main FunctionApp
-app.route(route="api/compress", methods=["POST"])(image_compressor)
+app.route(route="api/fileconversion", methods=["POST"])(image_compressor)
